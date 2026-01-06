0021518fe54d0f708675dd54df1015756eb54531
rityjacob <rityjacob@gmail.com>
2026-01-05 19:57:39 -0500

    added logger

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const logger = require('./Logger/logger');
 
 const app = express();
-
-
 PORT = process.env.PORT || 8000
 
-app.get('/',(req,res) =>{
-    res.send("hello news app");
+
+app.use(express.json());
+
+//logger
+app.use(logger)
+
+//Route
+
+
+app.get('/api/register',(req,res) =>{
+    res.send("hello news appp");
 })
 
 app.listen(PORT, () => console.log(`Server running in  PORT: ${PORT}`));
