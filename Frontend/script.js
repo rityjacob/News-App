c4b3d17740226e59a0c14efa379e55cbcdeee276
rityjacob <rityjacob@gmail.com>
2026-01-06 20:48:16 -0500

    connecting FE to BE

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,40 +1,86 @@
 
-    const dobInput = document.getElementById("dob");
-    const dobError = document.getElementById("dobError");
-    const form = document.getElementById("regForm");
-    const mobno = document.getElementById("mobileno");
-    const mobError = document.getElementById("mobError");
+const dobInput = document.getElementById("dob");
+const dobError = document.getElementById("dobError");
+const form = document.getElementById("regForm");
+const mobno = document.getElementById("mobileno");
+const mobError = document.getElementById("mobError");
+const regBtn = document.getElementById("regBtn");
 
 
-    // Block future dates
-    const today = new Date().toISOString().split("T")[0];
-    dobInput.max = today;
+// Block future dates
+const today = new Date().toISOString().split("T")[0];
+dobInput.max = today;
 
-    form.addEventListener("submit", function (e) {
-      const dob = new Date(dobInput.value);
-      const todayDate = new Date();
+form.addEventListener("submit", function (e) {
+    const dob = new Date(dobInput.value);
+    const todayDate = new Date();
 
-      let age = todayDate.getFullYear() - dob.getFullYear();
-      const monthDiff = todayDate.getMonth() - dob.getMonth();
+    let age = todayDate.getFullYear() - dob.getFullYear();
+    const monthDiff = todayDate.getMonth() - dob.getMonth();
 
-      if (
-        monthDiff < 0 ||
-        (monthDiff === 0 && todayDate.getDate() < dob.getDate())
-      ) {
-        age--;
-      }
+    if (
+    monthDiff < 0 ||
+    (monthDiff === 0 && todayDate.getDate() < dob.getDate())
+    ) {
+    age--;
+    }
 
-      if (age < 10) {
+    if (age < 10) {
+    e.preventDefault();
+    dobError.textContent = "Age must be at least 10 years.";
+    } else {
+    dobError.textContent = "";
+    }
+
+    if (!mobno.isInteger){
         e.preventDefault();
-        dobError.textContent = "Age must be at least 10 years.";
-      } else {
-        dobError.textContent = "";
-      }
+    mobError.textContent = "Mobile number must be integer";
+    } else {
+    mobError.textContent = "";
+    }
+});
 
-      if (!mobno.isInteger){
-         e.preventDefault();
-        mobError.textContent = "Mobile number must be integer";
-      } else {
-        mobError.textContent = "";
-      }
-    });
+
+form.addEventListener('click', async (e)=>{
+    e.preventDefault();
+
+    const usernameInput = document.querySelector('#username');
+    const passwordInput = document.querySelector('#password');
+    const rePass = document.querySelector('#confirmPassword');
+    if(passwordInput !== rePass){
+        alert(`Password doesnt match, please reenter`);
+        return;
+    }
+    const emailInput = document.querySelector('#email');
+    const mobNoInput = document.querySelector('mobileno');
+    const dobInput = document.querySelector('dob');
+
+
+
+
+    if(!username||password||rePass||email||mobNo||dob){
+        alert(`All fields are required`);
+        return;
+    }
+    
+    formData = {username,password,email,mobNo,dob};
+
+    const apiUrl = 'http://localhost:5001/api/register/'
+
+    try{
+        const response = await fetch(apiUrl,{
+            method:'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(formData),
+        });
+
+        if(response.ok){
+            alert(`Registered successfully`);
+        }
+
+
+
+    }catch{
+
+    }
+});
