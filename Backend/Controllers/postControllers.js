8fdc8b69589638d3d4b523a2ff771e1827113b6f
rityjacob <rityjacob@gmail.com>
2026-01-08 22:15:20 -0500

    post from registration works

--- Backend/Controllers/postControllers.js
+++ Backend/Controllers/postControllers.js
@@ -11,32 +11,28 @@ const regUser = async (req,res,next) => {
         return res.status(400).json({ success: false, msg: 'Request body is missing or Content-Type header not set' });
     }
     
-    try {
-        console.log(`Incoming req for new user`);
-        console.log("test 1");
-        const {username,password,email,mobno,dob} = req.body;
-        console.log("test 2");
-        console.log(req.body);
-        return res.status(201).json({ success: true, msg: 'User data received', data: req.body });
-    } catch (error) {
-        console.error('Controller error:', error);
-        return res.status(500).json({ msg: error.message });
-    }
-
-    // console.log(`Incoming req for new user`);
-
-    // console.log("test 1");
-    // const {username,password,email,mobno,dob} = req.body;
-    // console.log("test 2");
-
-    
-    // if(!username||!password||!email||!mobno||!dob){
-    //     return res.status(400).json({sucess: false, msg:'All fields are required'})
+    // try {
+    //     console.log(`Incoming req for new user`);
+    //     console.log("test 1");
+    //     const {username,password,email,mobno,dob} = req.body;
+    //     console.log("test 2");
+    //     console.log(req.body);
+    //     return res.status(201).json({ success: true, msg: 'User data received', data: req.body });
+    // } catch (error) {
+    //     console.error('Controller error:', error);
+    //     return res.status(500).json({ msg: error.message });
     // }
-    // console.log("test 3");
-    // console.log(req.body);
+
+    console.log(`Incoming req for new user`);
+
+    const {username,password,email,mobno,dob} = req.body;
     
-    // return res.status(200).json({ success: true, msg: 'User data received', data: req.body });
+    if(!username||!password||!email||!mobno||!dob){
+        return res.status(400).json({sucess: false, msg:'All fields are required'})
+    }
+    console.log(req.body);
+    
+    return res.status(200).json({ success: true, msg: 'User data received', data: req.body });
     
 
     // try{
