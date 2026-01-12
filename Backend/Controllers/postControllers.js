b38c666b6526663e43a683fc184539bc68937c4b
rityjacob <rityjacob@gmail.com>
2026-01-12 11:35:51 -0500

    registration successful

--- Backend/Controllers/postControllers.js
+++ Backend/Controllers/postControllers.js
@@ -10,19 +10,6 @@ const regUser = async (req,res,next) => {
     if (!req.body) {
         return res.status(400).json({ success: false, msg: 'Request body is missing or Content-Type header not set' });
     }
-    
-    // try {
-    //     console.log(`Incoming req for new user`);
-    //     console.log("test 1");
-    //     const {username,password,email,mobno,dob} = req.body;
-    //     console.log("test 2");
-    //     console.log(req.body);
-    //     return res.status(201).json({ success: true, msg: 'User data received', data: req.body });
-    // } catch (error) {
-    //     console.error('Controller error:', error);
-    //     return res.status(500).json({ msg: error.message });
-    // }
-
     console.log(`Incoming req for new user`);
 
     const {username,password,email,mobno,dob} = req.body;
@@ -32,23 +19,24 @@ const regUser = async (req,res,next) => {
     }
     console.log(req.body);
     
-    return res.status(200).json({ success: true, msg: 'User data received', data: req.body});
+    try{
+        
+        const post = await prisma.user.create({
+            data:{
+                username,
+                password,
+                email,
+                mobno,
+                dob: new Date(dob)}
+        })
     
 
-    // try{
-    //     const post = await prisma.post.create({
-    //         data:{
-    //             username,
-    //             password,
-    //             email,
-    //             mobno,
-    //             dob: new Date(dob)}
-    //     })
-    //     res.status(201).json(post);
+        res.status(201).json(post);
 
-    // }catch(error){
-    //     res.status(500).json(error)
-    // }
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error})
+    }
     
 }
 
