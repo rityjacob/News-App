4c069721260310d791d070241cd0c6259ec8d6cc
rityjacob <rityjacob@gmail.com>
2026-01-14 18:47:24 -0500

    Created login script

---
+++ Frontend/loginScript.js
@@ -0,0 +1,35 @@
+loginUrl = 'http://localhost:5001/api/login/'
+const loginForm = document.getElementById('loginForm');
+
+if(!loginForm){
+    loginForm.addEventListener('submit', async (e) =>{
+        e.preventDefault();
+
+        const username = document.querySelector('#username').value().trim();
+        const password = document.querySelector('#password').value().trim();
+
+        loginData = {username,password}
+
+        try{
+
+            const  response = await fetch(loginUrl,{
+                method:'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(loginData),
+            });
+            if(response.ok){
+                alert('Loggedin successfully')
+
+                username.value = '';
+                password.value = '';
+            }
+            else{
+                alert ('Login Unsuccesful')
+            }
+        }catch(error){
+            alert(`An error occured : ${error.message}`);
+
+        }
+
+    });
+}
